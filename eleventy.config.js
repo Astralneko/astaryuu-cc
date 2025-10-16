e3c73004ef3b00b768c9f2db627bc1ff8250dc09
Astralneko <90540803+Astralneko@users.noreply.github.com>
2025-10-16 14:36:09 -0500

    content can now be multicolumn :hotspot:

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,21 @@
 export default async function(eleventyConfig) {
 	// Shortcodes used in content templates
 	eleventyConfig.addPairedShortcode("textbox", async function(contents){
-		if (arguments.length > 1 && (arguments[1].toUpperCase() === "HASLINKS" || arguments[1].toUpperCase() === "HAS LINKS")) {
-			return `<div class=textbox>
+	const class_name = (typeof arguments[1] !== 'number' || arguments[1] === 0) ? "textbox" : `textbox grid-colspan-${arguments[1]}`
+		const last_argument = arguments.length - 1
+		if (arguments.length > 1 && (typeof arguments[last_argument] === 'string' || arguments[last_argument] instanceof String)) {
+			if (arguments[last_argument].toUpperCase() === "HASLINKS" || arguments[last_argument].toUpperCase() === "HAS LINKS") {
+				return `<div class="${class_name}">
 	<div class=textbox-inner>
 		${contents}
 	</div>`
-		} else {
-			return `<div class=textbox>
+			}
+		}
+		return `<div class="${class_name}">
 	<div class=textbox-inner>
 		${contents}
 	</div>
 </div>`
-		}
 	});
 	eleventyConfig.addPairedShortcode("textbox-link", async function(contents){
 		return `	<div class=textbox-links>
