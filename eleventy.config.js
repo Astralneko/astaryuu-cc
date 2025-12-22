eb13773fe14bd675e058cb7ce8c5e78fdef2c0d4
Astralneko <kokorovoid49@gmail.com>
2025-12-22 12:05:01 -0600

    add fas, fad, fal, fab to default

--- eleventy.config.js
+++ eleventy.config.js
@@ -34,11 +34,17 @@ export default async function(eleventyConfig) {
 	});
 	
 	eleventyConfig.addShortcode("oc-link-div", async function(name, filelink, characterlink, symbol1, symbol2, direction){
+		if (!(symbol1.startsWith("fas") || symbol1.startsWith("far") || symbol1.startsWith("fal") || symbol1.startsWith("fad") || symbol1.startsWith("fab"))) {
+			symbol1 = "fas fa-" + symbol1
+		}
+		if (!(symbol2.startsWith("fas") || symbol2.startsWith("far") || symbol2.startsWith("fal") || symbol2.startsWith("fad") || symbol2.startsWith("fab"))) {
+			symbol2 = "fas fa-" + symbol2
+		}
 		return `	<div class="textbox">
 		<div class="textbox-inner">
 			<a href="${characterlink}"><img src="${filelink}" width=40% style="float:${direction}; margin: 2.5%" class=image-link></a>
 			<h2 style="text-align:${direction}">${name}<br>
-			<i class="fas fa-${symbol1}"></i><i class="fas fa-${symbol2}"></i></h2>
+			<i class="${symbol1}"></i><i class="${symbol2}"></i></h2>
 		</div>
 	</div>`
 	})
